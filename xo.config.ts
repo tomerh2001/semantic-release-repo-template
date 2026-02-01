26ed31fa26b9b3a2ebec42daea2fcc7bf4050a04
Tomer Horowitz <tomerh@realcommerce.co.il>
2026-02-01 21:25:23 +0200

    fix: update dependencies and configuration files for improved compatibility

---
+++ xo.config.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { configs } from 'eslint-plugin-th-rules';
+
+const xoConfig = configs.recommendedTypescript;
+export default xoConfig;
